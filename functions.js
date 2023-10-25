f399c5ada9935c42ded6c5505cbda732da8867ec
Carlo Santos <rcarlosantos89@gmail.com>
2023-10-25 12:42:22 -0500

    reviewing javascript

---
+++ functions.js
@@ -0,0 +1,53 @@
+// console.log("hello world");
+// to run this file in your terminal navigate to the file and type node functions.js
+
+// 1. Write a function called add7 that takes one number and returns that number + 7.
+// parameters are the names listed in the function definition
+// you can think of parameters as variables that are accessible inside the function
+function add7(num) {
+  return num + 7;
+}
+
+function hello(name) {
+  console.log("hello " + name);
+}
+
+// after the function definition you can call the function
+// arguments are the values passed into the function
+// hello("Kenichi");
+// console.log(hello("Kenichi"));
+
+
+// add7(7); // 14
+// console.log(add7(7)); // 14
+
+// 2. Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// console.log(multiply(2, 6));
+
+// Data types?
+// String
+// Numbers
+// Boolean
+// Arrays
+// Objects
+// undefined
+// null
+
+
+// Loops?
+for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+
+// Condtionals
+// >, <, <=, >=, ===
+
+// Conditional operators
+// &&, ||
+
+// Assignment
+// =
